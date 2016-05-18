9e0a3e2eecb146984e3d7bcd7e37b1c37cf9383f
Alan Mac Kenna <alan.mackenna@serveit.ie>
2016-05-18 00:23:32 +0000

    build config changes

--- Build.bat
+++ Build.bat
@@ -14,10 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\StackifyLib.log4net\StackifyLib.log4net.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+REM %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\StackifyLib.log4net\StackifyLib.log4net.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net40
+REM mkdir Build
+REM mkdir Build\lib
+REM mkdir Build\lib\net40
 
-%nuget% pack "src\StackifyLib.log4net\StackifyLib.log4net.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "Src\StackifyLib.log4net\StackifyLib.log4net.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
